dbb0e7cb2925e03589fbb2cddbfa9303816c6eda
Connor Carpenter <connorcarpenter@gmail.com>
2015-09-06 23:55:49 -0600

    first commit

---
+++ UninformedSearch/UninformedSearch/BreadthFirstSearch.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using static UninformedSearch.BreadthFirstSearch;
+
+namespace UninformedSearch
+{
+    class BoardNode
+    {
+        public readonly BoardNode Previous;
+        public readonly TileBoard Board;
+        public char TileMoved;
+        public int Cost;
+
+        public BoardNode(BoardNode previous, TileBoard board, char TileMoved)
+        {
+            this.Previous = previous;
+            this.Board = board;
+            this.TileMoved = TileMoved;
+            if (previous == null)
+                Cost = 0;
+            else Cost = previous.Cost + 1;
+        }
+    }
+
+    interface INodeList
+    {
+        void Push(BoardNode node);
+        BoardNode Pop();
+    }
+
+    class NodeQueue : INodeList
+    {
+        private Queue<BoardNode> queue; 
+
+        public void Push(BoardNode node)
+        {
+            queue.Enqueue(node);
+        }
+
+        public BoardNode Pop()
+        {
+            return queue.Dequeue();
+        }
+    }
+
+    class NodeStack : INodeList
+    {
+        private Stack<BoardNode> stack;
+
+        public void Push(BoardNode node)
+        {
+            stack.Push(node);
+        }
+
+        public BoardNode Pop()
+        {
+            return stack.Pop();
+        }
+    }
+
+    class NodeDepthStack : INodeList
+    {
+        private Stack<BoardNode> stack;
+        private Queue<BoardNode> queue;
+        public int Depth;
+
+        public NodeDepthStack(int depth)
+        {
+            Depth = depth;
+        }
+
+        public void Push(BoardNode node)
+        {
+            if(node.Cost <= Depth)
+                stack.Push(node);
+            else
+                queue.Enqueue(node);
+        }
+
+        public BoardNode Pop()
+        {
+            if(stack.Count>0)
+                return stack.Pop();
+            else
+            {
+                SetDepth(Depth+1);
+                return stack.Pop();
+            }
+        }
+
+        public void SetDepth(int newDepth)
+        {
+            Depth = newDepth;
+            var newQueue = new Queue<BoardNode>();
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                if(node.Cost <= Depth)
+                    stack.Push(node);
+                else
+                    newQueue.Enqueue(node);
+            }
+
+            queue = newQueue;
+        }
+    }
+
+    class DepthFirstSearch : BreadthFirstSearch
+    {
+        public override void Solve(TileBoard inputBoard)
+        {
+            Solve(inputBoard, new NodeStack());
+        }
+    }
+
+    class BreadthFirstSearch
+    {
+        public virtual void Solve(TileBoard inputBoard)
+        {
+            Solve(inputBoard, new NodeQueue());
+        }
+
+        public static void Solve(TileBoard inputBoard, INodeList openNodes)
+        {
+            
+        }
+    }
+
+    
+}
---
+++ UninformedSearch/UninformedSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace UninformedSearch
+{
+    class Program
+    {
+        private const bool DEBUG = true;
+
+        static void Main()
+        {
+            TileBoard tileBoard;
+            if (!DEBUG)
+            {
+                var sr = new StreamReader("../../input.txt");
+                var input = sr.ReadToEnd();
+                input = input.Replace("\r\n", "");
+
+                tileBoard = new TileBoard(input);
+            }
+            else
+            {
+                tileBoard = new TileBoard();
+                tileBoard.Randomize('_');
+            }
+
+            BreadthFirstSearch.Solve(tileBoard.Copy());
+
+            Console.ReadLine();
+        }
+    }
+
+}
